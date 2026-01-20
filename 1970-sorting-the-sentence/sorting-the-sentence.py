f74a73ca21baad6e83d045bc1bbe98c34f48c7b2
REVATHY <148939084+Revathy333@users.noreply.github.com>
2026-01-20 14:22:20 +0530

    Time: 41 ms (36.26%) | Memory: 19.3 MB (13.19%) - LeetSync

--- 1970-sorting-the-sentence/sorting-the-sentence.py
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -1,3 +1,9 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        return " ".join(w[:-1] for w in sorted(s.split(), key=lambda x: x[-1]))
+        li = s.split(" ")
+        res = [""]*len(li)
+        print(li)
+        for w in li:
+            res[int(w[-1])-1] = w[:-1]
+        return " ".join(res)  
+              
