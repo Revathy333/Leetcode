259c6a7a86c3c151afbaf9308bcf0e420d392290
REVATHY <148939084+Revathy333@users.noreply.github.com>
2026-01-20 15:46:14 +0530

    Time: 3 ms (98.23%), Space: 19.5 MB (23.11%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,6 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for c in ransomNote:
-            if ransomNote.count(c) > magazine.count(c):
+        for i in ransomNote:
+            if i in magazine:
+                magazine = magazine.replace(i,"",1)
+            else:
                 return False
         return True
