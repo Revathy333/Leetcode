215868b1623db505aa2066637ccfee06bececed2
REVATHY <148939084+Revathy333@users.noreply.github.com>
2026-01-27 16:48:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (25.82%) - LeetHub

--- 2427-number-of-common-factors/2427-number-of-common-factors.py
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -1,8 +1,7 @@
 class Solution:
     def commonFactors(self, a: int, b: int) -> int:
         count = 0
-        x = min(a,b)
-        for i in range(1,x+1):
+        for i in range(1,min(a,b)+1):
             if a % i == 0 and b % i == 0:
                 count+=1
         return count        
